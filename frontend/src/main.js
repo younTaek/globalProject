7f7841e2769f688a574a0a9d154f352764a248cf
youngTaekVue <dudxor129@gmail.com>
2019-08-12 12:52:23 +0900

    20190812_update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue'
 import AOS from 'aos';
 import axios from 'axios'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'aos/dist/aos.css'; 
 
 Vue.prototype.$http = axios
